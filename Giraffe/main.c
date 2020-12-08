e0c43fe675fd309616e42db891046e2cc0e7ea8f
João Alonso <joaoalonso42@gmail.com>
2020-12-08 12:12:22 -0300

    switch statement

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -95,8 +95,30 @@ int main()
         return 0;
         printf("Answer: %f", cube(3.0));
 */
-
-
+/*Switch Statements
+    char grade;
+    printf("Enter your grade: ");
+    scanf("%c", &grade);
+    switch (grade){
+    case 'A':
+        printf("You did great! ");
+        break;
+    case 'B':
+        printf("You did alright! ");
+        break;
+    case 'C':
+        printf("You did poorly! ");
+        break;
+    case 'D':
+        printf("You did very bad! ");
+        break;
+    case 'F':
+        printf("You Failed! ");
+        break;
+    default:
+        printf("Invalid Grade");
+    }
+    */
 }
 /*Functions*/
     void sayHi(){
