81fb181984bc33194af6d343f7ac6f57505ab029
João Alonso <joaoalonso42@gmail.com>
2020-12-08 11:57:17 -0300

    calculator2.0

--- Giraffe/calculator/Calculator/main.c
+++ Giraffe/calculator/Calculator/main.c
@@ -5,14 +5,32 @@ int main()
 {
     double num1;
     double num2;
-    printf("Enter first number: ");
+    char opr;
+
+    printf("Enter a first number: ");
     scanf("%lf", &num1);
-    printf("Enter seclond number: ");
+    printf("Enter operator: ");
+    scanf(" %c", &opr);
+    printf("Enter second number: ");
     scanf("%lf", &num2);
 
-    printf("\nAnswer: %f", num1 + num2);
-    printf("\nAnswer: %f", num1 * num2);
-    printf("\nAnswer: %f", num1 - num2);
-    printf("\nAnswer: %f", num1 / num2);
+    if(opr == '+'){
+        printf("\nAnswer: %f", num1 + num2);
+    }
+        else if(opr == '-'){
+        printf("\nAnswer: %f", num1 - num2);
+    }
+        else if(opr == '*'){
+        printf("\nAnswer: %f", num1 * num2);
+    }
+        else if(opr == '/'){
+        printf("\nAnswer: %f", num1 / num2);
+        }else{
+            printf("invalid Operator");
+    }
+
+
+
+
     return 0;
 }
